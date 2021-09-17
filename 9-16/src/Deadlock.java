c79630bbddf6dffcbab637807303b493a0a27558
jonathan-wen-1 <90656665+jonathan-wen-1@users.noreply.github.com>
2021-09-17 13:50:50 -0400

    Update Deadlock.java

--- 9-16/src/Deadlock.java
+++ 9-16/src/Deadlock.java
@@ -4,15 +4,21 @@ public class Deadlock {
 	public static void main(String [] args) {
 		String subOne = "first";
 		String subTwo = "second";
-		
+		Object lock1 = new Object();
+		Object lock2 = new Object();
 		Runnable run1 = new Runnable() {
 			@Override
 			public void run() {
-				synchronized(subOne) { //first sync
-					System.out.println("Appending sub1 to sub2");
-					synchronized(subTwo) { //second sync
-						System.out.println(subTwo + subOne);//MAY sometimes print, but if deadlocked, will not
+				try {
+					synchronized(lock1) { //first sync
+						Thread.sleep(1000);//To ensure deadlock will happen, and let deadlockRun lock sub2 before continuing
+						System.out.println("Appending sub1 to sub2");
+						synchronized(lock2) { //second sync
+							System.out.println(subTwo + subOne);
+						}
 					}
+				}catch(InterruptedException e) {
+					System.out.println("Interrupted");
 				}
 				
 			}
@@ -22,12 +28,17 @@ public class Deadlock {
 
 			@Override
 			public void run() {
-				synchronized(subTwo) {//first sync, opposite parameter from run1
-					System.out.println("Appending sub2 to sub1");
-					synchronized(subOne) {//second sync, should cause deadlock because requesting 
-										  //the other value from run1 
-						System.out.println(subOne + subTwo);//MAY sometimes print, but if deadlocked, will not
+				try {
+					synchronized(lock2) {//first sync, opposite parameter from run1
+						Thread.sleep(1000);//To ensure deadlock
+						System.out.println("Appending sub2 to sub1");
+						synchronized(lock1) {//second sync, should cause deadlock because requesting 
+											  //the other value from run1 
+							System.out.println(subOne + subTwo);
+						}
 					}
+				}catch(InterruptedException e) {
+					System.out.println("Interrupted");
 				}
 			}
 			
@@ -35,14 +46,9 @@ public class Deadlock {
 		
 		new Thread(run1).start();
 		new Thread(deadlockRun).start();
-		/*
-		 * NOTE: this will not always cause deadlock, only when the two runnables try to execute exactly
-		 * parallel- this causes deadlock because in run1, the synchronized on sub2 is waiting on sub1 to 
-		 * finish, while the opposite is happening in deadlockRun. The deadlock occurs when synchronize in
-		 * run1 on subOne is finished and needs subTwo for the next synchronize, but deadlockRun is using
-		 * that subTwo and needs run1-- thus both runnables are waiting for the other to finish and cannot
-		 * continue unless it has the other
-		 */
+		//Deadlock happens because of Thread.sleep, which ensures that the running threads swap and both first layer 
+		//synchronized blocks will be called, thus s
+		//
 		System.out.println("Even though this is printed, code may not terminate");
 	}
 }
