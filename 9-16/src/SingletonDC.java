771007b47e593f9f9bc1beeb8fd2b745a30f7d92
jonathan-wen-1 <90656665+jonathan-wen-1@users.noreply.github.com>
2021-09-17 13:55:28 -0400

    Update SingletonDC.java

--- 9-16/src/SingletonDC.java
+++ 9-16/src/SingletonDC.java
@@ -2,14 +2,14 @@ package com.ss.java.sept16;
 
 public class SingletonDC {
 	volatile private static SingletonDC inst;
-	
+	static String lock = "lock"; //lock on this
 	private SingletonDC(){
 		//private constructor for singleton so it can't be called by anything
 	}
 	
 	public static SingletonDC getInstance() {
 		if(inst ==null) { //first unlocked check for if inst isn't null
-			synchronized(SingletonDC.class) {//synchronized for second locked check so if
+			synchronized(lock) {//synchronized for second locked check so if
 											 //multiple threads cannot instantiate the singleton in parallel
 				if(inst ==null) {
 					inst = new SingletonDC(); //if both checks pass then it will create only one
